ac092ac64d7200f039d7f66880a08d75cefbe0a1
Valentino Miotto <vmiotto@udesa.edu.ar>
2024-04-08 18:09:46 -0300

    controllers

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,15 @@
 const express = require("express");
 const router = express.Router();
 const mainController = require("../controllers/mainController");
+const logincontroller = require("../controllers/logincontroller");
+const productcontroller = require("../controllers/productcontroller");
+const profilecontroller = require("../controllers/profilecontroller");
+const registercontroller = require("../controllers/resgistercontroller");
+const searchcontroller = require("../controllers/searchcontroller");
 
 /* GET home page. */
 router.get("/", mainController.index);
+
+
 
 module.exports = router;
