838d6773a74327ab364af1a92f8a02cff7eddb15
Valentino Miotto <vmiotto@udesa.edu.ar>
2024-04-08 15:24:02 -0300

    archivos

---
+++ routes/index.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const router = express.Router();
+const mainController = require("../controllers/mainController");
+
+/* GET home page. */
+router.get("/", mainController.index);
+
+module.exports = router;
