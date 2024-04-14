0c1b9add92d00afd6ce331e8768a062badcd9d20
Valentino Miotto <vmiotto@udesa.edu.ar>
2024-04-14 19:34:56 -0300

    info autos

---
+++ db/db.js
@@ -0,0 +1,185 @@
+Productos = {
+    Autos: [
+        {
+            id: 1,
+            imagen: '',
+            nombre: 'Mercedes-Benz Clase S',
+            descripcion: 'El epítome del lujo y la tecnología automotriz.',
+            comentarios: [
+                {
+                    usuario: '@lujo_extremo',
+                    texto: '¡El Mercedes-Benz Clase S es simplemente sublime!',
+                    fotoDePerfil: ''
+                },
+                {
+                    usuario: '@auto_enthusiast',
+                    texto: 'Nada supera al Clase S en términos de lujo y confort.',
+                    fotoDePerfil: ''
+                }
+            ]
+        },
+        {
+            id: 2,
+            imagen: '',
+            nombre: 'Audi A8',
+            descripcion: 'Elegancia y rendimiento en su máxima expresión.',
+            comentarios: [
+                {
+                    usuario: '@petrolhead89',
+                    texto: 'El Audi A8 nunca deja de impresionar.',
+                    fotoDePerfil: ''
+                },
+                {
+                    usuario: '@carlover123',
+                    texto: '¡El A8 es una obra maestra de la ingeniería alemana!',
+                    fotoDePerfil: ''
+                }
+            ]
+        },
+        {
+            id: 3,
+            imagen: '',
+            nombre: 'BMW Serie 7',
+            descripcion: 'Un sedán de lujo que redefine el confort y la potencia.',
+            comentarios: [
+                {
+                    usuario: '@bmw_fanatic',
+                    texto: 'El Serie 7 es simplemente magnífico.',
+                    fotoDePerfil: ''
+                },
+                {
+                    usuario: '@german_engineering',
+                    texto: '¡BMW Serie 7, un sueño hecho realidad!',
+                    fotoDePerfil: ''
+                }
+            ]
+        },
+        {
+            id: 4,
+            imagen: '',
+            nombre: 'McLaren 720S',
+            descripcion: 'Innovación y rendimiento en un superdeportivo impresionante.',
+            comentarios: [
+                {
+                    usuario: '@speed_demon',
+                    texto: 'El McLaren 720S redefine el concepto de velocidad.',
+                    fotoDePerfil: ''
+                },
+                {
+                    usuario: '@mclaren_aficionado',
+                    texto: 'El McLaren 720S es simplemente impresionante.',
+                    fotoDePerfil: ''
+                }
+            ]
+        },
+        {
+            id: 5,
+            imagen: '',
+            nombre: 'Rolls-Royce Cullinan',
+            descripcion: 'El SUV de lujo definitivo, con una presencia imponente y comodidad insuperable.',
+            comentarios: [
+                {
+                    usuario: '@rolls_royce_lover',
+                    texto: 'El Cullinan es simplemente majestuoso en todos los aspectos.',
+                    fotoDePerfil: ''
+                },
+                {
+                    usuario: '@luxury_suv_enthusiast',
+                    texto: '¡El Rolls-Royce Cullinan redefine el segmento de los SUV de lujo!',
+                    fotoDePerfil: ''
+                }
+            ]
+        },
+        {
+            id: 6,
+            imagen: '',
+            nombre: 'Bentley Flying Spur',
+            descripcion: 'Lujo, elegancia y rendimiento se unen en este sedán excepcional.',
+            comentarios: [
+                {
+                    usuario: '@bentley_aficionado',
+                    texto: 'El Bentley Flying Spur es el epítome del refinamiento británico.',
+                    fotoDePerfil: ''
+                },
+                {
+                    usuario: '@luxury_car_collector',
+                    texto: '¡El Flying Spur es una obra maestra de la ingeniería automotriz!',
+                    fotoDePerfil: ''
+                }
+            ]
+        },
+        {
+            id: 7,
+            imagen: '',
+            nombre: 'Bugatti Divo',
+            descripcion: 'Diseñado para dominar las curvas y ofrecer una experiencia de conducción incomparable.',
+            comentarios: [
+                {
+                    usuario: '@bugatti_addict',
+                    texto: 'El Bugatti Divo es una bestia indomable en la carretera.',
+                    fotoDePerfil: ''
+                },
+                {
+                    usuario: '@speed_king',
+                    texto: '¡El Divo es la máxima expresión de la ingeniería automotriz!',
+                    fotoDePerfil: ''
+                }
+            ]
+        },
+        {
+            id: 8,
+            imagen: '',
+            nombre: 'Porsche 911 Turbo S',
+            descripcion: 'Un icónico deportivo con tecnología de vanguardia y un rendimiento excepcional.',
+            comentarios: [
+                {
+                    usuario: '@porsche_enthusiast',
+                    texto: 'El Porsche 911 Turbo S es una máquina impresionante en todos los sentidos.',
+                    fotoDePerfil: ''
+                },
+                {
+                    usuario: '@speed_freak',
+                    texto: '¡El 911 Turbo S siempre deja una sonrisa en mi rostro!',
+                    fotoDePerfil: ''
+                }
+            ]
+        },
+        {
+            id: 9,
+            imagen: '',
+            nombre: 'Aston Martin DB11',
+            descripcion: 'Un coupé elegante y potente que fusiona la artesanía británica con la tecnología de vanguardia.',
+            comentarios: [
+                {
+                    usuario: '@aston_martin_fanatic',
+                    texto: 'El Aston Martin DB11 es un símbolo de estilo y rendimiento.',
+                    fotoDePerfil: ''
+                },
+                {
+                    usuario: '@luxury_car_enthusiast',
+                    texto: '¡El DB11 es simplemente impresionante en todos los sentidos!',
+                    fotoDePerfil: ''
+                }
+            ]
+        },
+        {
+            id: 10,
+            imagen: '',
+            nombre: 'Lexus LC 500',
+            descripcion: 'Un gran turismo de lujo con un diseño cautivador y un rendimiento emocionante.',
+            comentarios: [
+                {
+                    usuario: '@lexus_enthusiast',
+                    texto: 'El Lexus LC 500 es una combinación perfecta de lujo y deportividad.',
+                    fotoDePerfil: ''
+                },
+                {
+                    usuario: '@performance_lover',
+                    texto: '¡El LC 500 ofrece una experiencia de conducción incomparable!',
+                    fotoDePerfil: ''
+                }
+            ]
+        }
+    ]
+    
+}
