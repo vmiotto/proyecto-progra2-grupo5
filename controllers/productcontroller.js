9b0d181aeb1c284a0f547ec827afe4b0ceaff4fa
Valentino Miotto <vmiotto@udesa.edu.ar>
2024-04-15 16:25:25 -0300

    act

--- controllers/productcontroller.js
+++ controllers/productcontroller.js
@@ -1,3 +1,4 @@
+const datos = require('../db/db')
 const controller = {
     index: function (req, res) {
       res.render("product", { title: "Express" });
--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const mainController = require("../controllers/mainController");
-/*const logincontroller = require("../controllers/logincontroller");
-const productcontroller = require("../controllers/productcontroller");
-const profilecontroller = require("../controllers/profilecontroller");
-const registercontroller = require("../controllers/registercontroller");
-const searchcontroller = require("../controllers/searchcontroller");
+
 
 /* GET home page. */
 router.get("/", mainController.index);
