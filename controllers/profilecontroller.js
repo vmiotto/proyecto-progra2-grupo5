f64c9fc70ad8037b91d9474bcc6bc5079c304437
Valentino Miotto <vmiotto@udesa.edu.ar>
2024-04-16 18:31:25 -0300

    a

--- controllers/profilecontroller.js
+++ controllers/profilecontroller.js
@@ -1,7 +1,11 @@
+const usuario = require("../db/infousuario")
 const controller = {
     index: function (req, res) {
       res.render("profile", { title: "Express" });
-    }
+    },
+    perfil:function (req, res) {
+      res.render("profile", { 'perfil': usuario });
+    },
   };
   
   module.exports = controller;
