423a69b479fc7f51a600a5d872cbe2e14472a3ca
Manuel Doiny <manudoiny@MacBook-Pro-de-Manuel-2.local>
2024-05-06 15:45:24 -0300

    config.js

---
+++ database/config/config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  "development": {
+    "username": "root",
+    "password": "root",
+    "database": "database_development",
+    "host": "127.0.0.1",
+    "dialect": "mysql"
+}
+}
