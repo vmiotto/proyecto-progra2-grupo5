c22a111360690c0ac674db6293d955cc0aa55dfc
manudoiny <mdoiny@udesa.edu.ar>
2024-06-10 15:58:38 -0300

    modelo del producto

---
+++ database/models/producto.js
@@ -0,0 +1,48 @@
+module.exports = function(sequelize, DataTypes) {
+    let alias = "Producto";
+    let cols = {
+        id: {
+            autoIncrement: true,
+            primaryKey: true,
+            type: DataTypes.INTEGER 
+        },
+        imagen: {
+            type: DataTypes.STRING
+        },
+        nombreProducto: {
+            type: DataTypes.STRING
+        },
+        descripcion: {
+            type: DataTypes.STRING
+        },
+        created_at: {
+            type: DataTypes.DATE
+        },
+        updated_at: {
+            type: DataTypes.DATE
+        },
+        deleted_at: {
+            type: DataTypes.DATE
+        }
+    }
+    let config = {
+        tableName: "productos", 
+        timestamps: true,
+        underscored: true 
+    }
+
+    const Producto = sequelize.define(alias, cols, config);
+
+    Producto.associate = function(models) {
+        Producto.belongsTo(models.usuario, {
+            as: "Usuario", // Alias del controlador
+            foreignKey: "usuario_id"
+        })
+        Producto.hasMany(models.comentario, {
+            as: "Producto",
+            foreignKey: "producto_id"
+        })
+    }
+
+    return Producto
+}
